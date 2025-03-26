637aa0da484143f7becae3a641188fe52578e1d9
Cesar Oliveira Jr <oliveiraugusto@ymail.com>
2025-03-25 22:27:09 -0300

    ..

--- src/js/scriptsCEP.js
+++ src/js/scriptsCEP.js
@@ -1,6 +1,40 @@
-const urlOficialCorreios = "";
 const urlViaCep = "https://viacep.com.br/ws/[[cep]]/json/";
 
-buscaCepCorreios(cep){
+function limpar() {
+    document.getElementsByClassName("inputCEP")[0].value = "";
+    document.getElementsByClassName("inputLogradouro")[0].value = "";
+    document.getElementsByClassName("inputComplemento")[0].value = "";
+    document.getElementsByClassName("inputNumero")[0].value = "";
+    document.getElementsByClassName("inputBairro")[0].value = "";
+    document.getElementsByClassName("inputLocalidade")[0].value = "";
+    document.getElementsByClassName("inputUF")[0].value = "";
 
+    document.getElementsByClassName("inputCEP")[0].focus();
+}
+
+function buscaCepXHR(cep){
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener("readystatechange", function() {
+    if(this.readyState === 4) {
+        console.log(this.responseText);
+    }
+    });
+
+    xhr.open("GET", urlViaCep.replace("[[cep]]", cep));
+
+    xhr.send();
+}
+
+function buscaCepFetch(){
+    const requestOptions = {
+        method: "GET",
+        redirect: "follow"
+      };
+      
+    fetch(urlViaCep.replace("[[cep]]", cep), requestOptions)
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.error(error));
 }
