66f733e4495bedd813c2d7f228af8929963c09e4
Cesar Oliveira Jr <cesar.a.oliveira@senacsp.edu.br>
2025-03-26 15:42:47 -0300

    set timeout

--- src/js/scriptsCEP.js
+++ src/js/scriptsCEP.js
@@ -12,29 +12,94 @@ function limpar() {
     document.getElementsByClassName("inputCEP")[0].focus();
 }
 
-function buscaCepXHR(cep){
-    var xhr = new XMLHttpRequest();
-    xhr.withCredentials = true;
-
-    xhr.addEventListener("readystatechange", function() {
-    if(this.readyState === 4) {
-        console.log(this.responseText);
-    }
-    });
-
-    xhr.open("GET", urlViaCep.replace("[[cep]]", cep));
-
-    xhr.send();
+function buscaCEP(tipo){   
+    hideOrShowLoading("show");
+    
+    let cep = document.getElementsByClassName("inputCEP")[0].value;
+    if(tipo == "JSON")
+        buscaCepFetch(cep);
+    else
+    buscaCepXMLHttpRequest(cep);
+    
+    setTimeout(function() {
+        hideOrShowLoading("hide");
+    }, 1000);
 }
 
-function buscaCepFetch(){
+function buscaCepXMLHttpRequest(cep) {
+    const url = urlViaCep.replace("[[cep]]", cep);
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true); // Configura a solicitação HTTP como assíncrona (true)
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 || xhr.status === 200) { // Verifica se a solicitação está concluída e Código 200 indica sucesso
+            const result = JSON.parse(xhr.responseText); // Converte o JSON da resposta em objeto
+            
+            document.getElementsByClassName("inputLogradouro")[0].value = result.logradouro || "";
+            document.getElementsByClassName("inputComplemento")[0].value = result.complemento || "";
+            document.getElementsByClassName("inputBairro")[0].value = result.bairro || "";
+            document.getElementsByClassName("inputLocalidade")[0].value = result.localidade || "";
+            document.getElementsByClassName("inputUF")[0].value = result.uf || "";
+
+            const inputs = ["inputLogradouro", "inputComplemento", "inputBairro", "inputLocalidade", "inputUF"];
+            inputs.forEach((inputClass) => {
+                document.getElementsByClassName(inputClass)[0].disabled = true;
+            });
+
+            document.getElementsByClassName("inputNumero")[0].focus();
+        } else {
+            console.error("Erro ao buscar o CEP: Status HTTP", xhr.status);
+        }
+    };
+
+    xhr.onerror = function () {
+        console.error("Erro na solicitação HTTP.");
+    };
+
+    xhr.send(); // Envia a solicitação
+}
+
+
+function buscaCepFetch(cep) {
     const requestOptions = {
         method: "GET",
         redirect: "follow"
-      };
-      
-    fetch(urlViaCep.replace("[[cep]]", cep), requestOptions)
-    .then((response) => response.text())
-    .then((result) => console.log(result))
-    .catch((error) => console.error(error));
+    };
+
+    let url = urlViaCep.replace("[[cep]]", cep);
+
+    fetch(url, requestOptions)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(`Erro: HTTP ${response.status}`);
+            }
+            return response.json(); // Convertendo resposta para JSON
+        })
+        .then((result) => {
+            document.getElementsByClassName("inputLogradouro")[0].value = result.logradouro || "";
+            document.getElementsByClassName("inputComplemento")[0].value = result.complemento || "";
+            document.getElementsByClassName("inputBairro")[0].value = result.bairro || "";
+            document.getElementsByClassName("inputLocalidade")[0].value = result.localidade || "";
+            document.getElementsByClassName("inputUF")[0].value = result.uf || "";
+
+            const inputs = ["inputLogradouro", "inputComplemento", "inputBairro", "inputLocalidade", "inputUF"];
+            inputs.forEach((inputClass) => {
+                document.getElementsByClassName(inputClass)[0].disabled = true;
+            });
+
+            document.getElementsByClassName("inputNumero")[0].focus();
+        })
+        .catch((error) => console.error("Erro ao buscar CEP:", error));
+}
+
+document.getElementsByClassName("inputCEP")[0].addEventListener('change', function() {
+    buscaCEP("XHR");
+});
+
+function hideOrShowLoading(tipo){
+    if(tipo == "hide")
+        document.getElementById("loading").style.display = "none";
+    else if(tipo == "show")
+    document.getElementById("loading").style.display = "block";
 }
